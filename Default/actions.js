b200138e2954c2ce84d4d443e3603cebb0178314
Aaron M <webgovernor@gmail.com>
2016-11-22 23:56:26 -0500

    Fix

--- Default/actions.js
+++ Default/actions.js
@@ -1,3 +1,14 @@
+_getNewTarget = function(current, newList) {
+    for(var i=0; i<newList.length; i++) {
+        var tgt = newList[i];
+        if (current == null || tgt.id != current.id) {
+            return tgt;
+        }
+    }
+    return null;
+}
+
+
 
 var actions = {
 
@@ -19,15 +30,6 @@ var actions = {
         taskMap[creep.memory.task](creep, target);
     },
 
-    _getNewTarget: function(current, newList) {
-        for(var i=0; i<newList.length; i++) {
-            var tgt = newList[i];
-            if (current == null || tgt.id != current.id) {
-                return tgt;
-            }
-        }
-        return null;
-    },
 
     doMelee: function(creep, target) {
         if(creep.attack(target) == ERR_NOT_IN_RANGE) {
@@ -43,7 +45,7 @@ var actions = {
         }
         else if (out == ERR_INVALID_TARGET) {
             console.log("ERROR: Invalid build target: " + target);
-            var tgt = this._getNewTarget(target, creep.room.memory.buildTargets);
+            var tgt = _getNewTarget(target, creep.room.memory.buildTargets);
             if (tgt != null)
                 creep.memory.targetId = tgt.id;
             else
@@ -60,7 +62,7 @@ var actions = {
             if (moveOK == ERR_NO_PATH) {
                 console.log("ERROR: No path to harvest source: " + target);
 
-                var tgt = this._getNewTarget(target, creep.room.memory.sources);
+                var tgt = _getNewTarget(target, creep.room.memory.sources);
                 if (tgt != null)
                     creep.memory.targetId = tgt.id;
                 else
@@ -95,7 +97,7 @@ var actions = {
             creep.moveTo(target);
         } else if (out == ERR_FULL || out == ERR_INVALID_TARGET) {
             console.log("ERROR: Full or invalid store target: " + target);
-            var tgt = this._getNewTarget(target, creep.room.memory.storeTargets);
+            var tgt = _getNewTarget(target, creep.room.memory.storeTargets);
             if (tgt != null)
                 creep.memory.targetId = tgt.id;
             else
