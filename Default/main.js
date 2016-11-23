ae38010e68c4cc8827153b466c0154b06c0cdf4c
nullism <webgovernor@gmail.com>
2016-11-23 11:34:18 -0700

    Added container storage to store role

--- Default/main.js
+++ Default/main.js
@@ -120,6 +120,7 @@ var doTick = function(spawn) {
             return (
                     structure.structureType == STRUCTURE_EXTENSION
                     || structure.structureType == STRUCTURE_SPAWN
+                    || structure.structureType == STRUCTURE_CONTAINER
                 ) &&
                 structure.energy > 0;
         }
